4f5443b5d881c4a74065a11777828d6678f4002a
rikschoonbeek <rik.schoonbeek@gmail.com>
2023-06-24 07:11:57 +0200

    Moved document fetching logic from context to hook

--- web_client/src/components/Document/DocumentsContext.js
+++ web_client/src/components/Document/DocumentsContext.js
@@ -1,24 +1,10 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useState } from 'react'
 import PropTypes from 'prop-types'
-import axios from 'axios'
-import { apiUrls } from '../../SystemConfig'
 
 export const DocumentsContext = createContext()
 
 export function DocumentsProvider({ children }) {
   const [documents, setDocuments] = useState([])
-
-  useEffect(() => {
-    axios
-      .get(apiUrls.document())
-      .then((response) => {
-        setDocuments(response.data)
-      })
-      .catch((error) => {
-        // TODO: handle error
-        console.error(error)
-      })
-  }, [])
 
   return (
     <DocumentsContext.Provider value={{ documents, setDocuments }}>
--- web_client/src/components/Document/useDocuments.js
+++ web_client/src/components/Document/useDocuments.js
@@ -1,8 +1,41 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
+
+import axios from 'axios'
+
+import { apiUrls } from '../../SystemConfig'
 import { DocumentsContext } from './DocumentsContext'
 
 const useDocuments = () => {
   const { documents, setDocuments } = useContext(DocumentsContext)
+
+  // TODO implement optional filter
+  const fetchDocuments = () => {
+    axios
+      .get(apiUrls.document())
+      .then((response) => {
+        setDocuments(response.data)
+      })
+      .catch((error) => {
+        // TODO: handle error
+        console.error(error)
+      })
+  }
+
+  useEffect(() => {
+    fetchDocuments()
+  }, [])
+
+  // const addDocument = (document) => {
+  //   axios
+  //     .post(apiUrls.document(), document)
+  //     .then((response) => {
+  //       // Initiate fetch updated documents
+  //     })
+  //     .catch((error) => {
+  //       // TODO: handle error
+  //       console.error(error)
+  //     })
+  // }
 
   return {
     documents,
